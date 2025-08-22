d8880f45df82d30ccdba41094f59ffa776a51a41
uuuppa <ekblom.skill@gmail.com>
2025-08-22 21:26:42 +0300

    Updated script to echo when done

--- new_generate_sprite.sh
+++ new_generate_sprite.sh
@@ -5,3 +5,6 @@ spreet -r 2 icons/ sprite-sheets/sprite
 
 # Generate retina sprites:
 spreet -r 4 icons/ sprite-sheets/sprite@2x
+
+# Print some useful info:
+echo "Generated sprite sheets successfully"
